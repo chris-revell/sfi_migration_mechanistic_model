40dbba522e9ae93f0f7e6a9e558fbc203143d086
Christopher Revell <cr395@cam.ac.uk>
2016-07-05 20:16:55 -0600

    Importing NDVI data and running over NDVI lattice

--- goose_sim.py
+++ goose_sim.py
@@ -1,56 +1,83 @@
 
+from sys import argv
 import numpy as np
 import random
 from math import exp
 
-#NDVI_import = np.array([])
+infilename = argv[1]
 
-#NDVI_file = open('filename','r')
+#File path for datafile given at command line
+#Use numpy.genfromtxt to import matrix in .txt file into array, skipping first row and column
+#Data is imported in string format and contains "NA" values for sea areas.
+#Also in data file, greener points have higher NDVI values.
+#We want the potential to be lower at greener areas, so we multiply every value by -1 when using as a potential
+infile = open(infilename,'r')
 
-#for line in NDVI_file:
-#    line_stripped = line.rstrip()
-#    line_array    = line_stripped.split(" ")
-    #Add line to data array
-#    NDVI_import = np.vstack([NDVI_import,line_array])
+#Number of columns and rows in data file
+nrows=0
+ncols=0
+for line in infile:
+    nrows = nrows+1
+    if nrows == 2:
+        ncols = len(line.split(' '))
+infile.close()
 
-#Should now have NDVI values imported into array NDVI_import
+#Import data matrix
+NDVI_import = np.genfromtxt(infilename, dtype=str, skip_header=1, usecols=range(1,ncols), delimiter=' ')
 
+#Convert to number of columns and rows in array.
+nrows=nrows-1
+ncols=ncols-1
 
-outfile = open('test_data.txt','w')
-outfile2 = open('potentials.txt','w+b')
+#Define position of breeding ground and initial position of goose
+breeding_position = (0,ncols-1)
+goose_position = (int(random.random()*1000),int(random.random()*1000))
 
-NDVI_import = np.zeros((10,10))
-boltzmann_factors = np.zeros((10,10))
+#Also define array to hold Boltzmann factors with same dimensions as data array
+boltzmann_factors = np.zeros(NDVI_import.shape)
+#Calculate values for all elements of boltzmann factor array.
+#If the corresponding element in the data array is "NA", the Boltzmann factor is set to 0.
+for x in range(0,nrows):
+    for y in range(0,ncols):
+        if NDVI_import[x,y] == "NA":
+            boltzmann_factors[x][y] = 0
+        else:
+            #Define distance of (x,y) from breeding position - put this into a function?
+            r_i_vector = [(breeding_position[0]-x),(breeding_position[1]-y)]
+            r_i        = (np.vdot(r_i_vector,r_i_vector))**0.5+0.1
+            #Define potential at (x,y) from NDVI data and "breeding location gravity"
+            #Note that factor of -1 in exponential is included in "potential" value
+            potential = (float(NDVI_import[x,y]) + 0.1/r_i)/1000
+            boltzmann_factors[x][y] = exp(potential)
 
-breeding_position = [9,9]
-goose_position = [0,0]
+#We have now defined the Boltzmann factor array that will be used to determine probabilities as the goose moves through the lattice.
 
-for x in range (0,10):
-    for y in range (0,10):
-        r_i_vector = [(breeding_position[0]-x),(breeding_position[1]-y)]
-        r_i        = (np.vdot(r_i_vector,r_i_vector))**0.5+0.1
-        NDVI_import[x,y] = random.random() - 0.1/r_i
-        boltzmann_factors[x,y] = exp(-1.0*NDVI_import[x,y])
+#Open file into which results are printed
+outfile = open(infilename[0:-4]+'_goose_positions.txt','w')
 
-#Now have an array of random potential values tilted by attractor at (9,9)
-
-for t in range (0,200):
+#Loop over timesteps
+for t in range (0,100000):
+    #Define possible_states array to hold the neighbouring lattice points that a goose can move into. Generally 9, fewer at system edges.
     possible_states = []
+    #Identify current neighbouring elements
+    #Do not include values outside the bounds of the array
     for x in range(-1,2):
-        if 0 <= (goose_position[0]+x) <= 9:
-            #Do not include neighbours outside bounds or array
+        if 0 <= (goose_position[0]+x) < nrows:
             for y in range(-1,2):
-                if 0 <= (goose_position[1]+y) <= 9:
+                if 0 <= (goose_position[1]+y) < ncols :
                     possible_states.append((goose_position[0]+x,goose_position[1]+y))
                 else:
                     pass
         else:
             pass
 
+    #Sum Boltzmann factors for possible states
     boltzmann_sum = 0
     for z in possible_states:
         boltzmann_sum = boltzmann_sum+boltzmann_factors[z]
 
+    #Use a random number generator and probabilities defined by Boltzmann factors
+    #to decide which lattice point the goose moves to next
     probability_sum = 0
     random_number = boltzmann_sum*random.random()
     for p in possible_states:
@@ -61,12 +88,22 @@ for t in range (0,200):
         else:
             pass
 
+    #Now that we have updated the goose position, write it to output file
     output = str(t)+'  '+str(goose_position[0])+'  '+str(goose_position[1])+'\n'
-#    outfile.write(output)
-#    print(goose_position)
+    outfile.write(output)
 
-final_outfile = open('finalgooseposition.txt','w')
-final_outfile.write(str(goose_position[0])+'  '+str(goose_position[1]))
+print (boltzmann_factors[goose_position[0]-1,goose_position[1]-1],boltzmann_factors[goose_position[0]-1,goose_position[1]],boltzmann_factors[goose_position[0]-1,goose_position[1]+1])
+print (boltzmann_factors[goose_position[0],goose_position[1]-1],boltzmann_factors[goose_position[0],goose_position[1]],boltzmann_factors[goose_position[0],goose_position[1]+1])
+print (boltzmann_factors[goose_position[0]+1,goose_position[1]-1],boltzmann_factors[goose_position[0]+1,goose_position[1]],boltzmann_factors[goose_position[0]+1,goose_position[1]+1])
+print (NDVI_import[goose_position[0]-1,goose_position[1]-1],NDVI_import[goose_position[0]-1,goose_position[1]],NDVI_import[goose_position[0]-1,goose_position[1]+1])
+print (NDVI_import[goose_position[0],goose_position[1]-1],NDVI_import[goose_position[0],goose_position[1]],NDVI_import[goose_position[0],goose_position[1]+1])
+print (NDVI_import[goose_position[0]+1,goose_position[1]-1],NDVI_import[goose_position[0]+1,goose_position[1]],NDVI_import[goose_position[0]+1,goose_position[1]+1])
+#final_outfile = open('finalgooseposition.txt','w')
+#final_outfile.write(str(goose_position[0])+'  '+str(goose_position[1]))
 
-np.savetxt(outfile2,NDVI_import)
-np.savetxt(outfile2,boltzmann_factors)
+#outfile2 = open('NDVI_import.txt','w+b')
+#outfile3 = open('boltzmann_factors.txt','w+b')
+#np.savetxt(outfile2,NDVI_import)
+#np.savetxt(outfile3,boltzmann_factors)
+#outfile.close()
+#print(ncols,nrows)
