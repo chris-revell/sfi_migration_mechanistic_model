f3e5138dc95b45dda0d4e1bc9b53325b34b5bbc5
Christopher Revell <cr395@cam.ac.uk>
2016-07-15 14:37:47 +0100

    Initiating neighbours-only interpolation.
    Needs testing.

--- goose_sim.py
+++ goose_sim.py
@@ -13,13 +13,16 @@ import datetime
 importfolderpath = argv[1]
 
 #Set system parameters
-t_max = 5000  # Total number of timesteps
+t_max = 12  # Total number of timesteps
 #t_halfmonth =   # NDVI data comes as one file for every half month, so we need only specify a number of timesteps per half month and provide a finite set of months for a run.
 A     = 40000   # Prefactor for breeding site gravitational attraction
 kT    = 1000    # Measure of goose temperature or "restlessness"
 #Define position of breeding ground and initial position of goose
 breeding_position = (279,1147)
 goose_position    = (495,560)
+
+
+
 
 #From folder path proveded at command line, find list of files to import NDVI data from.
 #Each file corresponds to half a month. "isfile" checks that we find only files, not directories.
@@ -80,6 +83,7 @@ boltzmann_factors = np.zeros((nrows-1,ncols-1))
 r_i_array         = np.zeros((nrows-1,ncols-1))
 NDVI_gradient     = np.empty((nrows-1,ncols-1),dtype=float)
 NDVI_interpolated = np.empty((nrows-1,ncols-1),dtype=str)
+time_updated      = np.zeros((nrows-1,ncols-1),dtype=int)
 
 #Fill r_i_array with distances from breeding site.
 for x in range(0,nrows-1):
@@ -93,33 +97,16 @@ for x in range(0,nrows-1):
 initialfilename = importfolderpath+'/'+datafiles.pop(0)
 print (initialfilename)
 NDVI_next = np.genfromtxt(initialfilename, dtype=str, skip_header=1, usecols=range(1,ncols), delimiter=' ')
+possible_states = []
 
-
-#Define functional form of breeding site gravity potential
-def breeding_gravity(radius):
-    return (A/radius)
-
-def boltzmann_update():
-    global boltzmann_factors
-    global NDVI_interpolated
-    for x in range(0,nrows-1):
-        for y in range(0,ncols-1):
-            if NDVI_interpolated[x,y] == "NA":
-                boltzmann_factors[x][y] = 0
-            else:
-                #Define potential at (x,y) from NDVI data and "breeding location gravity"
-                #Note that factor of -1 in exponential is included in "potential" value
-                potential = (float(NDVI_interpolated[x,y]) + breeding_gravity(r_i_array[x,y]))/kT
-                boltzmann_factors[x,y] = exp(potential)
-
-#Define subroutine to update the position of the goose
-def system_update():
-    global boltzmann_factors
-    global goose_position
-    global outfile
-
+def find_possible_states():
     #Define possible_states array to hold the neighbouring lattice points that a goose can move into. Generally 9, fewer at system edges.
-    possible_states = []
+    global possible_states
+
+    #Start by removing all elements in previous possible states list in case the number of elements in the new list is smaller
+    for element in possible_states:
+        del element
+
     #Identify current neighbouring elements
     #Do not include values outside the bounds of the array
     for x in range(-1,2):
@@ -131,6 +118,29 @@ def system_update():
                     pass
         else:
             pass
+
+#Define functional form of breeding site gravity potential
+def breeding_gravity(radius):
+    return (A/radius)
+
+def boltzmann_update(possible_states):
+    global boltzmann_factors
+    global NDVI_interpolated
+    for element in possible_states:
+        if NDVI_interpolated[element] == "NA":
+            boltzmann_factors[x][y] = 0
+        else:
+            #Define potential at (x,y) from NDVI data and "breeding location gravity"
+            #Note that factor of -1 in exponential is included in "potential" value
+            potential = (float(NDVI_interpolated[element]) + breeding_gravity(r_i_array[element]))/kT
+            boltzmann_factors[element] = exp(potential)
+
+#Define subroutine to update the position of the goose
+def system_update():
+    global boltzmann_factors
+    global goose_position
+    global outfile
+    global possible_states
 
     #Sum Boltzmann factors for possible states
     boltzmann_sum = 0
@@ -155,10 +165,11 @@ def system_update():
 
 #Function to import a new NDVI file and redefine the previous "next" file as the new "current" file.
 #Calculates the gradient array between the two files.
-def importnext():
+def importnext(t):
     global NDVI_interpolated
     global NDVI_gradient
     global NDVI_next
+    global time_updated
     NDVI_interpolated = NDVI_next
     filenameatinterval = importfolderpath+'/'+datafiles.pop(0)
     NDVI_next = np.genfromtxt(filenameatinterval, dtype=str, skip_header=1, usecols=range(1,ncols), delimiter=' ')
@@ -170,35 +181,44 @@ def importnext():
             else:
                 NDVI_gradient[x,y] = (int(NDVI_next[x,y])-int(NDVI_interpolated[x,y]))/update_interval
     print (t, filenameatinterval)
+    #Change the value of the time updated for every element of the array.
+    time_updated.fill(t-1)
 
 #Function to interpolate NDVI data between two files
-def interpolate():
+def interpolate(possible_states,t):
     global NDVI_gradient
     global NDVI_interpolated
+    global time_updated
     #Loop over all elements in array
-    for x in range(0,nrows-1):
-        for y in range(0,ncols-1):
+    for element in possible_states:
+        if NDVI_interpolated[element] == "NA":
             #Do nothing for NA values (which correspond to sea)
-            if NDVI_interpolated[x,y] == "NA":
-                pass
+            pass
+        else:
             #Use gradient array to update interpolated NDVI array to next state.
-            else:
-                newvalue = float(NDVI_interpolated[x,y]) + NDVI_gradient[x,y]
-                NDVI_interpolated[x,y] = str(newvalue)
+            newvalue = float(NDVI_interpolated[element]) + (t-time_updated[element])*NDVI_gradient[x,y] #(t-time_updated[element]) gives the time that has elapsed since that component of the array was last interpolated, and therefore the magnitude of the prefactor required when adding some multiple of the gradient
+            NDVI_interpolated[element] = str(newvalue)
+        #Update the value for the time last updated for each component of the possible states list.
+        time_updated[element] = t
 
 
+#Find possible states for first run of system
+find_possible_states()
 #Loop over timesteps
 for t in range (0,t_max):
     print(t)
     #For every import interval, import a new file into NDVI_next and redefine NDVI_interpolated to hold the old values of NDVI_next
     if (int(t%update_interval) == 0):
-        importnext()
+        importnext(t)
 
     #Update system state according to current interpolated NDVI values and corresponding BOltzmann factors.
     system_update()
 
+    #Find possible states for next run of system
+    find_possible_states()
+
     #Update the interpolated NDVI array
-    interpolate()
+    interpolate(possible_states,t)
 
     #Update Boltzmann factors according to the new interpolated NDVI values
-    boltzmann_update()
+    boltzmann_update(possible_states)
