b9a68da115319609ca178ce17ae451fe91a00f35
Christopher Revell <cr395@cam.ac.uk>
2016-11-12 18:50:43 +0000

    Wind data fixes

--- seabird_sim.py
+++ seabird_sim.py
@@ -12,8 +12,8 @@ datafiles = [os.path.join(argv[1],f) for f in os.listdir(argv[1]) if f[-4:].lowe
 
 currentposition = (500,720)
 kT = 100
-t_max=200
-A = 1
+t_max=500
+A = 100
 
 #Import ground map
 earth = np.genfromtxt(datafiles[0],delimiter=",")
@@ -40,7 +40,7 @@ d_latlong = 180/resources_shape[0]
 resources_filtered = np.zeros(resources_shape)
 for i in range(0,resources_shape[0]):
     for j in range(0,resources_shape[1]):
-        if 99999 > resources[i,j] > 5:
+        if 99999 > resources[i,j] > 1:
             resources_filtered[i,j] = resources[i,j]
 
 output_data_store = np.zeros((t_max,2))
@@ -74,7 +74,7 @@ for t in range(1,t_max):
     possible_state_boltzmann_factors = np.zeros((3,3))
     for i in range(-1,2):
         for j in range(-1,2):
-            state_index     = ((currentposition[0]+i),(currentposition[1]+j))
+            state_index = ((currentposition[0]+i),(currentposition[1]+j))
             if i == 0 and j == 0:
                 pass
             elif earth[state_index] == 1:
@@ -86,14 +86,18 @@ for t in range(1,t_max):
                         if earth[k,l] == 0 and resources_filtered[k,l] > 0:
                             state_potential = state_potential + resources_filtered[k,l]/realdistance(state_index,currentposition)
 
-
                 wind_vector = np.array([wind_merid[currentposition],wind_zonal[currentposition]]) #In form [y,x] for ease of translation to np arrays.
                 wind_magnitude = sqrt(np.dot(wind_vector,wind_vector))
                 displacement_vector = np.array([i,j])
-
+                print("currentposition",currentposition)
+                print("wind_vector",wind_vector)
+                print("wind_magnitude",wind_magnitude)
+                print("displacement_vector",displacement_vector)
+                print("np.dot(wind_vector,displacement_vector)",np.dot(wind_vector,displacement_vector))
                 state_potential = state_potential + A*wind_magnitude*np.dot(wind_vector,displacement_vector)
 
                 possible_state_boltzmann_factors[i+1,j+1] = exp(state_potential/kT)
+                print(i,j,state_potential)
 
     #Update position
     #Sum Boltzmann factors for possible states
@@ -110,7 +114,7 @@ for t in range(1,t_max):
         for j in range(0,3):
             probability_sum = probability_sum + possible_state_boltzmann_factors[i,j]
             if random_number < probability_sum:
-                currentposition = tuple(map(sum, zip(currentposition, (i-1,j-1))))
+                currentposition = (currentposition[0]+i-1,currentposition[1]+j-1) #tuple(map(sum, zip(currentposition, (i-1,j-1))))
                 break
             else:
                 pass
@@ -145,19 +149,15 @@ cax = ax2.imshow(resources_filtered,cmap="GnBu")
 cbar = fig2.colorbar(cax)
 ax2.plot(output_data_store[:,1],output_data_store[:,0])
 ax2.tick_params(axis='both',which='both',bottom='off',top='off',labelbottom='off')
-ax2.set_xrange([0,resources_shape[1]])
-ax2.set_yrange([resources_shape[0],0])
-ax2.set_yrange
+ax2.set_xlim([0,resources_shape[1]])
+ax2.set_ylim([resources_shape[0],0])
 fig2.savefig(os.path.join(run_folder,"lattice_chloro.pdf"))
 
 fig3 = plt.figure()
 ax3 = fig3.add_subplot(212)
 ax3.imshow(earth,cmap="Greys")
 ax3.plot(output_data_store[:,1],output_data_store[:,0])
-ax3.tick_params(
-    axis='x',          # changes apply to the x-axis
-    which='both',      # both major and minor ticks are affected
-    bottom='off',      # ticks along the bottom edge are off
-    top='off',         # ticks along the top edge are off
-    labelbottom='off')
+ax3.tick_params(axis='both',which='both',bottom='off',top='off',labelbottom='off')
+ax2.set_xlim([0,resources_shape[1]])
+ax2.set_ylim([resources_shape[0],0])
 fig3.savefig(os.path.join(run_folder,"lattice_earth.pdf"))
